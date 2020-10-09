e55d95fbade033ebb3e304f8dcc83e1c7b37cc9c
kevin <kevinntech@nate.com>
2020-10-09 11:06:34 +0900

    스프링 MVC 설정 까지

--- demo-boot-web/src/main/java/me/kevinntech/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/me/kevinntech/demobootweb/SampleController.java
@@ -1,15 +1,32 @@
 package me.kevinntech.demobootweb;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SampleController {
 
+    /*  1) preHandle 1
+        2) preHandle 2
+        3) 요청 처리 (핸들러)
+        4) postHandle 2
+        5) postHandle 1
+        6) 뷰 렌더링
+        7) afterCompletion 2
+        8) afterCompletion 1    */
+
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello " + person.getName();
+    }
+
+    @GetMapping("/message")
+    public @ResponseBody String message(@RequestBody String body){
+        return body;
+    }
+
+    @GetMapping("/jsonMessage")
+    public Person jsonMessage(@RequestBody Person person){ // HTTP 요청 본문으로 들어오는 JSON 문자열을 Person 객체로 변환
+        return person; // 그리고 그대로 리턴함
     }
 
 }
--- demo-boot-web/src/main/java/me/kevinntech/demobootweb/WebConfig.java
+++ demo-boot-web/src/main/java/me/kevinntech/demobootweb/WebConfig.java
@@ -1,9 +1,39 @@
 package me.kevinntech.demobootweb;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setPackagesToScan(Person.class.getPackageName());
+        return jaxb2Marshaller;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 명시적으로 순서를 지정하고 싶다면 아래와 같이 할 수 있다. (낮을수록 우선순위 ↑)
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/hi")
+                .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
 
 }
